e15f66fba7503b430f712c2a728698e3c05cc9c3
ibrahim-ih1518019 <ih1518019@qu.edu.qa>
2020-09-03 13:20:48 +0300

    Project Files

---
+++ files/app/build/generated/source/buildConfig/androidTest/debug/com/cmps312/courseregistration/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cmps312.courseregistration.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cmps312.courseregistration.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
